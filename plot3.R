aa30491dad8286e2b77d50ece1d3907058ca64d8
rhao22 <raphaelh@gmail.com>
2015-02-02 23:15:12 +0100

    add plot 3 files

---
+++ plot3.R
@@ -0,0 +1,30 @@
+dat = read.csv("household_power_consumption.txt", sep = ";", header = TRUE)
+
+dat$Time <- strptime(paste(dat$Date , dat$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+dat$Date <- as.Date(strptime(dat$Date, "%d/%m/%Y"))
+
+start_date <-as.Date(strptime("01/02/2007", "%d/%m/%Y"))
+end_date <-as.Date(strptime("02/02/2007", "%d/%m/%Y"))
+
+
+subset_dat <- dat[which((dat$Date==start_date) | (dat$Date==end_date)), ]
+  
+plot(subset_dat$Time, as.numeric(as.character(subset_dat$Sub_metering_1)),
+     type="n",
+     xlab="",
+     ylab="Energy sub metering"
+)
+lines(subset_dat$Time, as.numeric(as.character(subset_dat$Sub_metering_1)))
+lines(subset_dat$Time, as.numeric(as.character(subset_dat$Sub_metering_2)), col="red")
+lines(subset_dat$Time, as.numeric(as.character(subset_dat$Sub_metering_3)), col="blue")
+
+
+legend("topright",
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=c(1,1,1),
+       col=c("black", "blue","red")
+       ) 
+
+
+dev.copy(png, file = "plot3.png")  ## Copy my plot to a PNG file
+dev.off()  ## Don't forget to close the PNG device!
