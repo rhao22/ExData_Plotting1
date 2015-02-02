599870a7030ced66b0a9d51caa055df5297717e8
rhao22 <raphaelh@gmail.com>
2015-02-02 22:36:41 +0100

    PLot 1 files

---
+++ plot1.R
@@ -0,0 +1,15 @@
+dat = read.csv("household_power_consumption.txt", sep = ";", header = TRUE)
+dat$Date <- as.Date(strptime(dat$Dat, "%d/%m/%Y"))
+
+start_date <-as.Date(strptime("01/02/2007", "%d/%m/%Y"))
+end_date <-as.Date(strptime("02/02/2007", "%d/%m/%Y"))
+
+
+subset_dat <- dat[which((dat$Date==start_date) | (dat$Date==end_date)), ]
+  
+hist(as.numeric(as.character(subset_dat$Global_active_power)), 
+     col="red", 
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatt)"
+     )
+
